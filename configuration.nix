da05203fd500eea9b28c3c0062cac4d0aa2a7a16
hayden-donnelly <hayden.git@proton.me>
2024-03-31 13:51:35 -0400

    Removed comments from config to save vertical space.

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,6 @@ in
 {
     imports =
         [
-            # Include the results of the hardware scan.
             ./hardware-configuration.nix
             ./home.nix
             ./cachix.nix
@@ -16,7 +15,6 @@ in
 
     home-manager.useGlobalPkgs = true;
 
-    # Bootloader.
     boot = {
         loader = {
             systemd-boot.enable = true;
@@ -30,14 +28,12 @@ in
         kernelParams = [ "i915.force_probe=4680" ];
     };
 
-    # Allow unfree packages
     nixpkgs.config = {
         allowUnfree = true;
     };
     
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-    # Enable OpenGL
     hardware.opengl = {
         enable = true;
         driSupport = true;
@@ -45,13 +41,11 @@ in
         extraPackages = with pkgs; [ intel-media-driver ];
     };
 
-    # Configure X11.
     services.xserver = {
         enable = true;
         layout = "us";
         xkbVariant = "";
         videoDrivers = ["nvidia"];
-        # Enable the KDE Plasma Desktop Environment.
         displayManager.sddm.enable = true;
         desktopManager.plasma5.enable = true;
     };
@@ -71,53 +65,26 @@ in
     };
 
     hardware.nvidia = {
-        # Modesetting is required.
         modesetting.enable = true;
-
-        # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
         powerManagement.enable = false;
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
         powerManagement.finegrained = false;
-
-        # Use the NVidia open source kernel module (not to be confused with the
-        # independent third-party "nouveau" open source driver).
-        # Support is limited to the Turing and later architectures. Full list of
-        # supported GPUs is at:
-        # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-        # Only available from driver 515.43.04+
-        # Do not disable this unless your GPU is unsupported or if you have a good reason to.
         open = true;
-
-        # Enable the Nvidia settings menu,
-        # accessible via `nvidia-settings`.
         nvidiaSettings = true;
-
-        # Optionally, you may need to select the appropriate driver version for your specific GPU.
         package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
 
-    networking.hostName = "nixos"; # Define your hostname.
-
-    # Enable networking
+    networking.hostName = "nixos";
     networking.networkmanager.enable = true;
-
-    # Set your time zone.
     time.timeZone = "America/Toronto";
-
-    # Select internationalisation properties.
     i18n.defaultLocale = "en_CA.UTF-8";
 
-    # Enable virtualisation for Docker.
     virtualisation.docker = {
         enable = true;
         enableNvidia = true;
     };
 
-    # Enable CUPS to print documents.
     services.printing.enable = true;
 
-    # Enable sound with pipewire.
     sound.enable = true;
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
@@ -128,16 +95,8 @@ in
         pulse.enable = true;
         # If you want to use JACK applications, uncomment this
         #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
     };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-    # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.hayden = {
         isNormalUser = true;
         description = "Hayden";
