7c78a10ee86514cc5687025c128715bbdb3d11fe
hayden-donnelly <hayden.git@proton.me>
2024-03-31 13:06:56 -0400

    Added glsl syntax highlighting to neovim.

--- home.nix
+++ home.nix
@@ -110,6 +110,7 @@ in
                 }
                 vim-tmux-navigator
                 nvim-web-devicons
+                vim-glsl
             ];
             
             extraLuaConfig = ''
