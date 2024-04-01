0ccca7014f5e6db577572a82e5222953ae74f33e
hayden-donnelly <hayden.git@proton.me>
2024-04-01 14:06:00 -0400

    Tweaked clangd setup.

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -1,2 +1,12 @@
 local nvim_lsp = require('lspconfig')
-nvim_lsp.clangd.setup {}
+
+nvim_lsp.clangd.setup {
+    handlers = {
+        ["textDocument/publishDiagnostics"] = function() end,
+    },
+    init_options = {
+        highlight = {
+            lsRanges = false,
+        }
+    },
+}
