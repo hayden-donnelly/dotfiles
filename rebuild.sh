6a4c4c1d25d8337ad10b7c4eb223e3393e602d29
hayden-donnelly <donnellyhd@outlook.com>
2024-06-25 19:10:29 -0400

    Switched config to flakes and upgraded to 24.05.

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo nixos-rebuild switch -I nixos-config=configuration.nix
+#sudo nixos-rebuild switch -I nixos-config=configuration.nix
+sudo nixos-rebuild switch --flake .#nixos
